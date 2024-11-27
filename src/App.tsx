976d102d3f4bfe999fce7996c41c515b09724606
mtsuts <mariamtsuts@gmail.com>
2024-11-26 19:01:42 -0800

    added dependencies

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          Delivery in U.S. map Vizualization
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
